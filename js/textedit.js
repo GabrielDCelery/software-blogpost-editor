22dc4e043ee74e9f78bb485ad8dd6b4bd4d1f69b
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2015-12-15 13:17:28 +0100

    Added the basic styling and basic functionalities to blogpost editor, like making text bold, italic, underline, font-style and font-weight

--- js/textedit.js
+++ js/textedit.js
@@ -1,2 +1,58 @@
+(function(){
 
-console.log('textedit.js is running...');
+$(document).ready(function(){
+
+/******************************************************************************************
+Declaring variables
+******************************************************************************************/
+
+	var $textEditorContent = $('#textEditorContent');
+	var $functionButtons = $('.functionButton');
+	var $functionSelects = $('.functionSelect');
+
+/******************************************************************************************
+Binding events
+******************************************************************************************/
+
+	$functionButtons.on('mousedown', buttonDown);
+	$functionButtons.on('mouseup', buttonUp);
+	$functionButtons.on('mouseleave', buttonUp);
+	$functionSelects.on('change', changeSelection);
+	$functionSelects.on('mouseleave', changeSelection);
+
+/******************************************************************************************
+Functions
+******************************************************************************************/
+
+	function formatDoc(contentArea, commandName, argumentValue) {
+		document.execCommand(commandName, false, argumentValue);
+		contentArea.focus();
+	}
+
+	function buttonDown(){
+		var clickedDiv = $(this);
+		var id = clickedDiv.attr('id');
+		id = id.replace('functionButton-', '');
+		clickedDiv.addClass('clicked');
+		formatDoc($textEditorContent, id);
+	}
+
+	function buttonUp(){
+		var clickedDiv = $(this);
+		clickedDiv.removeClass('clicked');
+	}
+
+	function changeSelection(){
+		var selectedOption = $(this);
+		var selectedSelect = selectedOption.closest('select');
+		var id = selectedSelect.attr('id');
+		id = id.replace('functionSelect-', '');
+		var argumentValue = selectedOption.val();
+		formatDoc($textEditorContent, id, argumentValue);
+	}
+
+
+})
+
+}())
+
